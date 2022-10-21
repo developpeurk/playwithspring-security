b086db37494be3bee635b44fe8907f66e7655785
developpeurk <developpeurk@gmail.com>
2022-10-21 19:31:56 +0100

    Remember Me Cookie and Extra Options

--- src/main/java/com/example/demospringsecurity1/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demospringsecurity1/security/ApplicationSecurityConfig.java
@@ -14,6 +14,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
+import java.util.concurrent.TimeUnit;
+
 import static com.example.demospringsecurity1.security.ApplicationUserPermission.COURSE_WRITE;
 import static com.example.demospringsecurity1.security.ApplicationUserRole.*;
 
@@ -43,7 +45,7 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .loginPage("/login").permitAll()
                 .defaultSuccessUrl("/courses", true)
                 .and()
-                .rememberMe();
+                .rememberMe().tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21)).key("somethingverysecured");
     }
 
     @Override
